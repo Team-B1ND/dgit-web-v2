71be262e9b26594297af19ac4fd1927398697782
Cartridge0923 <pjmin0923@gmail.com>
2025-12-16 00:24:30 +0900

    feat: global styles

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import ThemeProvider from '@components/common/provider/themeProvider'
 import Layout from '@src/components/common/layout'
+import GlobalStyles from '@src/styles/globalStyles'
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -9,6 +10,7 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <ThemeProvider>
+      <GlobalStyles/>
       <Layout>
         <Outlet />
       </Layout>
