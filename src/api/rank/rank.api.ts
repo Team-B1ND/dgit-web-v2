923e512956388f722a8c02592df12331e926c160
Cartridge0923 <pjmin0923@gmail.com>
2025-12-18 21:21:29 +0900

    feat: commit rank #10

--- src/api/rank/rank.api.ts
+++ src/api/rank/rank.api.ts
@@ -6,7 +6,8 @@ export const getRankApi = async <T extends RankScope>(
   scope: "commit" | "repository" | "streak" | "total",
 ) => {
   const { data } = await customAxios.get<BaseResponse<GetRankType<T>[]>>(
-    `/ranking/${scope}`
+    `/ranking/${scope}`,
+    { skipAuth: true }
   );
   return data;
 };
--- src/routes/rank_/_rankLayout/commit/index.tsx
+++ src/routes/rank_/_rankLayout/commit/index.tsx
@@ -1,9 +1,15 @@
+import CommitRank from '@src/pages/rank/commit'
 import { createFileRoute } from '@tanstack/react-router'
+import { Suspense } from 'react'
 
 export const Route = createFileRoute('/rank_/_rankLayout/commit/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/rank/_rankLayout/commit/"!</div>
+  return (
+    <Suspense fallback={<>loading....</>}>
+      <CommitRank/>
+    </Suspense>
+  )
 }
