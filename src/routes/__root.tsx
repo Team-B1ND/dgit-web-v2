cda70cb6aa881b3f417290caf0ebd628ca338b93
Cartridge0923 <pjmin0923@gmail.com>
2025-12-18 12:38:23 +0900

    feat: add query provider on root #10

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,7 @@ import { Outlet, createRootRoute } from '@tanstack/react-router'
 import ThemeProvider from '@components/common/provider/themeProvider'
 import Layout from '@src/components/common/layout'
 import GlobalStyles from '@src/styles/globalStyles'
+import QueryProvider from '@src/components/common/provider/queryProvider'
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -9,11 +10,13 @@ export const Route = createRootRoute({
 
 function RootComponent() {
   return (
-    <ThemeProvider>
-      <GlobalStyles/>
-      <Layout>
-        <Outlet />
-      </Layout>
-    </ThemeProvider>
+    <QueryProvider>
+      <ThemeProvider>
+        <GlobalStyles/>
+        <Layout>
+          <Outlet />
+        </Layout>
+      </ThemeProvider>
+    </QueryProvider>
   )
 }
