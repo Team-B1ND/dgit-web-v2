86235dd1d41c0dbd49aa7441795d214c975670ea
Cartridge0923 <pjmin0923@gmail.com>
2025-12-18 09:19:28 +0900

    feat: static profile #9

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -4,9 +4,50 @@ import * as S from './style'
 const ProfilePage = () => {
   return (
     <S.ProfileLayoutContainer>
-      <ProfileHeader/>
+      <ProfileHeader />
+      <S.ProfileMainContainer>
+        <S.ProfileRecordContainer>
+          <S.ProfileRecordItem>
+            <S.ProfileRecordRow>
+              <p>오늘의 커밋</p>
+              <span>{0}회</span>
+            </S.ProfileRecordRow>
+            <S.ProfileRecordRow>
+              <p>이번주 커밋</p>
+              <span>{0}회</span>
+            </S.ProfileRecordRow>
+          </S.ProfileRecordItem>
+          <S.ProfileRecordItem>
+            <p>기록</p>
+            <S.ProfileRecordRow>
+              <p>레포지토리</p>
+              <span>{0}개</span>
+            </S.ProfileRecordRow>
+            <S.ProfileRecordRow>
+              <p>총 커밋</p>
+              <span>{0}커밋</span>
+            </S.ProfileRecordRow>
+            <S.ProfileRecordRow>
+              <p>최장 스트릭</p>
+              <span>{0}일</span>
+            </S.ProfileRecordRow>
+          </S.ProfileRecordItem>
+        </S.ProfileRecordContainer>
+        <S.ProfileBigRecordContainer>
+          <S.ProfileBigRecordItem>
+            <p>랭킹</p>
+            <div>{120}위</div>
+            <span>상위 {24}%</span>
+          </S.ProfileBigRecordItem>
+          <S.ProfileBigRecordItem>
+            <p>레벨</p>
+            <div>{20}LV</div>
+            <span>{1000} / {2400} EXP</span>
+          </S.ProfileBigRecordItem>
+        </S.ProfileBigRecordContainer>
+      </S.ProfileMainContainer>
     </S.ProfileLayoutContainer>
-  )
+  );
 }
 
 export default ProfilePage
--- src/pages/profile/style.ts
+++ src/pages/profile/style.ts
@@ -1,3 +1,4 @@
+import { DodamShape, DodamTypography } from "@b1nd/dds-web";
 import styled from "styled-components";
 
 export const ProfileLayoutContainer = styled.div`
@@ -6,3 +7,75 @@ export const ProfileLayoutContainer = styled.div`
   gap: 20px;
   flex-grow: 1;
 `
+
+export const ProfileMainContainer = styled.div`
+  display: flex;
+  gap: 20px;
+`
+
+export const ProfileRecordContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 320px;
+`
+
+export const ProfileRecordItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 24px;
+  gap: 8px;
+  background-color: ${({ theme }) => theme.backgroundNormal};
+  ${DodamShape.Large};
+  > p {
+    ${DodamTypography.Heading1.Bold};
+  }
+`;
+
+export const ProfileRecordRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > p {
+    ${DodamTypography.Body1.Medium};
+    color: ${({ theme }) => theme.labelAssistive};
+  }
+
+  > span {
+    ${DodamTypography.Body1.Bold};
+  }
+`
+
+export const ProfileBigRecordContainer = styled.div`
+  display: flex;
+  gap: 20px;
+  width: 100%;
+`
+
+export const ProfileBigRecordItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 24px;
+  background-color: ${({ theme }) => theme.backgroundNormal};
+  ${DodamShape.Large};
+  flex-grow: 1;
+  > p {
+    ${DodamTypography.Heading1.Bold};
+  }
+  
+  > div {
+    display: flex;
+    height: 120px;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    flex-grow: 1;
+    ${DodamTypography.Heading1.Bold};
+  }
+
+  > span {
+    ${DodamTypography.Label.Regular};
+    color: ${({ theme }) => theme.labelAssistive};
+  }
+`;
