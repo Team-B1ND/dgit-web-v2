4069c9e4d3355700b7fe7c7feabbef91a54efea8
Cartridge0923 <pjmin0923@gmail.com>
2025-12-22 14:45:03 +0900

    feat: add background props on needLoginWarn #9

--- src/components/common/exception/needLoginWarn/index.tsx
+++ src/components/common/exception/needLoginWarn/index.tsx
@@ -1,14 +1,19 @@
-import { DodamFilledButton } from "@b1nd/dds-web";
+import { DodamFilledButton, DodamShape } from "@b1nd/dds-web";
 import { loginWithDauth } from "@src/libs/login/login";
 import { useRouterState } from "@tanstack/react-router";
 import styled from "styled-components"
 
-const NeedLoginWarn = () => {
+interface NeedLoginWarnProps {
+  background?: boolean
+}
+const NeedLoginWarn = ({
+  background = false
+}: NeedLoginWarnProps) => {
   const pathname = useRouterState({
     select: (state) => state.location.pathname,
   });
   return (
-    <NeedLoginWarnContainer>
+    <NeedLoginWarnContainer $background={background.toString()}>
       로그인이 필요합니다!
       <DodamFilledButton text="로그인하러 가기" size="Medium" width={240} textTheme="staticWhite" onClick={() => loginWithDauth(pathname)} />
     </NeedLoginWarnContainer>
@@ -18,13 +23,17 @@ const NeedLoginWarn = () => {
 export default NeedLoginWarn
 
 
-const NeedLoginWarnContainer = styled.div`
+const NeedLoginWarnContainer = styled.div<{
+  $background: string
+}>`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100%;
   flex-grow: 1;
+  ${DodamShape.Large};
+  background-color: ${({ $background, theme }) => $background === "true" ? theme.backgroundNormal : ""};
   color: ${({ theme }) => theme.labelAssistive};
   gap: 12px;
 `;
