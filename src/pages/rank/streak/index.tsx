56b00b23aadaffbf9cce91534e02befb83ad0291
Cartridge0923 <pjmin0923@gmail.com>
2025-12-22 15:54:28 +0900

    feat: use CustomText on rank tables

--- src/pages/rank/streak/index.tsx
+++ src/pages/rank/streak/index.tsx
@@ -1,5 +1,6 @@
 import Avatar from "@src/components/common/avatar";
 import Table from "@src/components/common/table";
+import CustomText from "@src/components/rank/customText";
 import { STREAK_RANK_TABLE } from "@src/constants/rank/rank.constant";
 import { useGetRankQuery } from "@src/queries/rank/rank.queries";
 
@@ -9,8 +10,8 @@ const StreakRank = () => {
     <Table
       keys={STREAK_RANK_TABLE}
       data={data.data.map((item) => [
-        item.rank,
-        item.longestStreak,
+        <CustomText text={"#" + item.rank} color="primaryNormal" weight="Bold" />,
+        <CustomText text={item.longestStreak.toString()} color="primaryNormal" weight="Bold" />,
         <Avatar url={item.avatarUrl} size="28px" />,
         item.username,
         item.name,
--- src/pages/rank/total/index.tsx
+++ src/pages/rank/total/index.tsx
@@ -1,5 +1,6 @@
 import Avatar from "@src/components/common/avatar";
 import Table from "@src/components/common/table";
+import CustomText from "@src/components/rank/customText";
 import { TOTAL_RANK_TABLE } from "@src/constants/rank/rank.constant";
 import { useGetRankQuery } from "@src/queries/rank/rank.queries";
 
@@ -9,9 +10,9 @@ const TotalRank = () => {
     <Table
       keys={TOTAL_RANK_TABLE}
       data={data.data.map((item) => [
-        item.rank,
-        item.level,
-        item.totalCommits,
+        <CustomText text={"#" + item.rank} color="primaryNormal" weight="Bold" />,
+        <CustomText text={item.level.toString()} color="primaryNormal" weight="Bold" />,
+        <CustomText text={item.totalCommits.toString()} color="primaryNormal" weight="Bold" />,
         <Avatar url={item.avatarUrl} size="28px" />,
         item.username,
         item.name,
