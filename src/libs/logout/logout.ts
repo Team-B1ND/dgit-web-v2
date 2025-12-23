74f324be6f5b56f634ba53a68d34c531cf23670b
Cartridge0923 <pjmin0923@gmail.com>
2025-12-23 19:46:04 +0900

    feat: check not login state

--- src/libs/logout/logout.ts
+++ src/libs/logout/logout.ts
@@ -1,7 +1,14 @@
+import { B1ndToast } from "@b1nd/b1nd-toastify";
+import { ACCESS_TOKEN_KEY } from "@src/constants/token.constants";
 import token from "@src/libs/token/token";
 
 export const logout = () => {
-  alert("로그아웃되었습니다.")
-  token.clearToken();
-  location.reload();
+  const haveToken = !!token.getToken(ACCESS_TOKEN_KEY)
+  if (haveToken) {
+    alert("로그아웃되었습니다.")
+    token.clearToken();
+    location.reload();
+  } else {
+    B1ndToast.showError("비로그인 상태입니다!")
+  }
 }
