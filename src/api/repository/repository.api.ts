7a28894ec8b3c53158c85ad32f215e51ae231d7b
Cartridge0923 <pjmin0923@gmail.com>
2025-12-23 16:59:19 +0900

    feat: register repository api #27

--- src/api/repository/repository.api.ts
+++ src/api/repository/repository.api.ts
@@ -2,7 +2,7 @@ import customAxios from "@src/libs/axios/customAxios"
 import { BaseResponse } from "@src/types/baseResponse";
 import { RegisterRepositoryResType, RegisterRepositoryType } from "@src/types/repository/repository.type";
 
-export const registerRepository = async (repoData: RegisterRepositoryType ) => {
+export const registerRepositoryApi = async (repoData: RegisterRepositoryType ) => {
   const { data } = await customAxios.post<BaseResponse<RegisterRepositoryResType>>(`/repository/register`, repoData);
   return data
 }
